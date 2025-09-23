700f345382b79d22c153f77143f50b85105020e4
221RDB362 <122871094+Krissx680@users.noreply.github.com>
2025-09-23 14:18:12 +0300

    Update TextPrimaryButton.tsx

--- src/TextPrimaryButton.tsx
+++ src/TextPrimaryButton.tsx
@@ -1,5 +1,7 @@
 import { Button, ButtonProps } from "@mui/material";
 import { typography } from "./typography";
+import { brand, neutral } from "./theme/colorTokens";
+
 
 interface TextPrimaryButtonProps extends ButtonProps {
   children: React.ReactNode;
@@ -13,7 +15,7 @@ export const TextPrimaryButton = ({
     <Button
       variant="text"
       sx={{
-        color: "#0068A9",
+        color: brand[500],
         fontFamily: typography.fontFamily,
         ...typography["button.Large"],
         textTransform: "none",
@@ -21,9 +23,17 @@ export const TextPrimaryButton = ({
         padding: "12px 24px",
         backgroundColor: "transparent",
         boxShadow: "none",
-        "&:hover": {
-          backgroundColor: "rgba(188, 217, 255, 0.3)",
-          color: "#387CBB",
+         "&:hover": {
+          backgroundColor: "rgba(188, 217, 255, 0.3)", 
+          color: brand[300], 
+        },
+        "&:active": {
+          backgroundColor: "rgba(0, 104, 169, 0.2)", 
+          color: brand[300],
+        },
+        "&:focus": {
+          outline: "none",
+          boxShadow: "none",
         },
       }}
       {...props}
